1ab39741f6bae6603073e2bbdca20e3f4ba7a88e
Yogesh Bansal <yogeshbansal.stackgeeks@gmail.com>
2021-05-10 20:04:16 +0530

    second

--- Feed.js
+++ Feed.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View , TouchableOpacity, Modal, TextInput, FlatList , Image , SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View , TouchableOpacity, Modal, TextInput, FlatList , Image , SafeAreaView, ImageBackground } from 'react-native';
 
 class Feed extends Component {
 constructor(){
@@ -10,18 +10,11 @@ constructor(){
         val1 : "" ,
         val2  : '',
         Vis : false,
-        myArr : [{}]
+        arrData2 : [{
+          title : "",
+          images : "" }],
     }
 }
-
-
-
- items = [
-    { title : "something" , imgUrl : "hello"},
-    ]
-
-    
-
  onmode = () =>{
    this.setState({Modale: true})
   }
@@ -30,37 +23,46 @@ constructor(){
    this.setState({Modale : false})
   }
   
- onSubmit = (itm) => {
+ onSubmit = (data) => {
 
-   this.setState({myArr : this.state.myArr[itm.index].title=this.state.val1})
-   console.log(this.state.myArr , "hello thi is my arr")
+
+  this.setState({arrData2 : [...this.state.arrData2, {title : this.state.val1 , images : this.state.val2}]})
+  console.log(this.state.arrData, "show data")
+
    this.setState({Vis : true})
-     console.log(this.state.val1 , "getting")
-     console.log(this.state.val2 , "link")
     this.setState({Modale : false})
   }
 
+  onEdit = () =>{
+ console.log("Edit")
+  }
+  onDelete = () =>{
+    console.log("Delete") 
+  }
+
+
   render(){
   
-   var showData = (itm)=>{
-     console.log(itm,"itm")
-  return (
+   var showData = (data )=>{
+     console.log(data,"data to shown")
+     console.log(data.index,"index")
+       return (
     <SafeAreaView>
     <View style={styles.container}>
     <View style = {styles.head}>
-      <Image source={require('./assets/man.png')} style = {{height:45 , width : 45 , left : 35 , top : 38 }}  />
+      <Image source={require('./assets/man.png')} style = {{height:45 , width : 45 , left : 20 , top : 38 }}  />
     
-      <Text style = {{left : 90 , bottom : 45, fontWeight : 'bold' , top : -8}}> StackGeeks  {'\n'} 1m .</Text>
+      <Text style = {{left : 75 , bottom : 45, fontWeight : 'bold' , top : -10}}> StackGeeks  {'\n'} 1m .</Text>
   
-      <Image source={require('./assets/friends.png')} style = {{height:15 , width : 15 , left : 125  , bottom : 24 }}  />
+      <Image source={require('./assets/friends.png')} style = {{height:12 , width : 12 , left : 110  , bottom : 23 }}  />
       <TouchableOpacity onPress = {this.onmode} style = {styles.buttons}>
        <Image source={require('./assets/add.png') } style = {{height : 40 , width : 40}}   />
        </TouchableOpacity>
        </View>
 
     <View style = {{flex : 4 }}>
-    <Text style = {{ left : 10 ,color : 'black'   }}>{this.state.myArr.title}{'\n'}</Text>
-    <Image source= {{uri : this.items.title }} style = {{flex : 4, width : 400 , height : 500 }} />   
+    <Text style = {{ left : 20 ,marginBottom : 15 ,color : 'black' , fontWeight :'bold'   }}>{data.item.title}</Text>
+    <Image source= {{uri : data.item.images }} style = {{flex : 4, width : 400 , height : 500 }} />   
      </View>
      { this.state.Vis ? 
     <View style = {{flex : 0.4 , backgroundColor : 'white' , width : 360 , flexDirection : 'row'}} >
@@ -84,35 +86,44 @@ constructor(){
     </View>
     </View> : null
      }
+     </View>
 
     <Modal visible = {this.state.Modale} >
-    <View  style = {{flex : 1  , paddingTop : 40 , backgroundColor : 'gray' } }>
-    <Text style = {styles.textVw}>Enter Text</Text>
-    <TextInput placeholder = "Enter Text to show "  style = {styles.placehold} onChangeText ={(val1) => this.setState({val1})}></TextInput>
-    <Text style = {styles.textVw}>Enter Image Url</Text>
-    <TextInput placeholder ="Enter Image Url"  style = {styles.placehold} onChangeText ={(val2) => this.setState({val2})}></TextInput>
-    <View style = {{flex : 1 , flexDirection : 'row' ,  top : 60 , margin : 20 }}>
-    <View style = {{flex : 0.5 , borderRadius : 30 ,  marginRight : 30  }}>
-      <TouchableOpacity onPress = {()=>this.onSubmit(itm)} style = {{backgroundColor : 'green' , height : 30 , justifyContent :'center' ,  borderRadius : 20}}>
-        <Text style = {{textAlign : 'center'  ,  fontWeight : 'bold'}}>Submit</Text>
+   <View style = {{backgroundColor : 'lightgreen' , height : 600 , borderRadius : 50, marginTop : 80}}>
+    <View style = {styles.modaal}>
+    <View  style = {{ top : 60 } }>
+    <Text style = {{ fontWeight : 'bold' , left : 10}} >TITLE</Text>
+    <TextInput placeholder = "Enter TITLE "  style = {styles.placehold} onChangeText ={(val1) => this.setState({val1})}></TextInput>
+     </View>
+    <View style = {{ top : 100  } }>
+    <Text  style = {{ fontWeight : 'bold' ,  left : 10}}>IMAGE</Text>
+    <TextInput placeholder ="Enter  URL"  style = {styles.placehold} onChangeText ={(val2) => this.setState({val2})}></TextInput>
+    </View>
+   
+    <View >
+      <TouchableOpacity onPress = {()=>this.onSubmit(data) } style = {{backgroundColor : 'green' , height : 40 , justifyContent :'center' ,  borderRadius : 10 , width : 230 ,left : 10 , top : 180 , borderLeftWidth : 1 , borderRightWidth : 1 , borderBottomWidth : 1 }}>
+        <Text style = {{textAlign : 'center'  ,  fontWeight : 'bold' ,color : 'white' , fontSize : 15}}>Submit</Text>
       </TouchableOpacity>
       </View>
-      <View style = {{flex : 0.5  , height : 50 ,  borderRadius : 30}}>
-      <TouchableOpacity onPress = {this.offmode} style = {{backgroundColor : 'red',  height : 30 , justifyContent :'center' , borderRadius : 20}} >
-        <Text style = {{textAlign : 'center', color : "white" , fontWeight : 'bold'}}>Cancel</Text>
-      </TouchableOpacity>
       </View>
+     
+      <View style = {{left : 200 , bottom : 90}}>
+      <TouchableOpacity onPress = {this.offmode} style = {{ bottom : 340 , left : 105 }} >
+        <Image source = {require('./assets/cross2.png')}  style = {{height : 25 , width : 25 }}         />
+      </TouchableOpacity>
     </View>
     </View>
+    
+  
     </Modal>
 
-     </View>
+    
      </SafeAreaView>);
     }
     return(
         <FlatList
-      data={this.state.myArr}
-      renderItem ={(itm)=>showData(itm)} >
+      data={this.state.arrData2} 
+      renderItem ={(data)=>showData(data)} >
         </FlatList>
         )
     
@@ -133,18 +144,18 @@ const styles = StyleSheet.create({
         left : 335
   
       },
-      textVw : {
-    left : 20
-      },
       placehold : {
+         height : 40,
+         width : 250,
+         borderRadius : 15,
          textAlign : 'center',
-         bottom : 22,
-         borderWidth : 1,
-         borderRadius : 30,
-         height : 30,
-         width : 220,
-         left : 130
+         backgroundColor : 'lightgray',
+         borderBottomWidth : 1,
+         borderRightWidth : 1,
+         borderLeftWidth : 1,
+         fontWeight : 'bold'
       },
+      
       head :{
   
         flex : 0.5 ,
@@ -159,6 +170,15 @@ const styles = StyleSheet.create({
   bottom : 18,
   fontWeight : 'bold'
   
+      },
+      modaal : {
+        height : 400,
+        width : 300,
+        padding : 20,
+        margin : 50,
+        marginTop : 100,
+        backgroundColor : 'white',
+        borderRadius : 50
       }
   });
   
