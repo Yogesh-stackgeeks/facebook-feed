f59433af5c859e032407e21b73c0135b14d02a0b
Yogesh-stackgeeks <83753293+Yogesh-stackgeeks@users.noreply.github.com>
2021-05-14 20:47:18 +0530

    Add files via upload

--- Feed.js
+++ Feed.js
@@ -10,89 +10,99 @@ class Feed extends Component {
       modale: false,
       val1: "",
       val2: '',
-      viewButtons: false,
-      arrData: [{
-        title: "",
-        images: ""
-      }]
+      pressed : false,
+      bgColor: "gray",
+      index : 0,
+      arrData: [""],
+      pressed : false
 
     }
   }
-
   onMode = () => {
     this.setState({ modale: true })
+
   }
 
   offMode = () => {
     this.setState({ modale: false })
+    
   }
 
   onSubmit = () => {
-
-    this.setState({ 
-      arrData: [{ title: this.state.val1, images: { uri: this.state.val2 } }, ...this.state.arrData],
-      modale: false, viewButtons: true, id: this.state.id
-    });
-    console.log(this.state.id, "fetching id ")
-    console.log(this.state.arrData, "show data")
-
+    const newData = [...this.state.arrData]
+    newData.push({id : Math.random().toString() , title : this.state.val1 , images : this.state.val2})
+    this.setState((prevState)=>{
+      return {
+        arrData : [...newData],
+        modale : false,
+      }
+    })
   }
+ 
+  
 
+  onEdit = ( data) => {
+    console.log(this.state.index)
+    let store = this.state.arrData
+    store[this.state.index] = {title : this.state.val1 , images : this.state.val2}
 
-  onEdit = (index) => {
-    this.setState({ modale: true })
-    // console.log(this.state.arrData[index] , "title fetch")
-    this.setState({ val1: this.state.arrData[index].title, val2: this.state.arrData[index].images })
+    this.setState({arrData : store , modale : false})
 
-    console.log(this.state.val1, this.state.val2, "values fetched")
-
-    console.log(index, "edited")
   }
 
   onDelete = (index) => {
-    console.log("Delete")
-
     this.state.arrData.splice(index, 1)
     alert("Deleted Succesfully")
   }
-
+  onLike = (index) => {
+    console.log("Liked", index)
+     this.setState({bgColor : 'red'})
+    
+  }
   render() {
 
     return (
       <View>
+      <View style = {{flex : 0 , height : 90}}>
+      <TouchableOpacity onPress={this.onMode} style={styles.buttons}>
+                    <Image source={imagePath.add} style={{ height: 40, width: 40 }} />
+                  </TouchableOpacity>
+                   </View>
         <FlatList
           data={this.state.arrData}
-          keyExtractor={(data) => data.id}
+          keyExtractor = {(item,index)=> index.toString()}
           renderItem={({ item, index }) =>
             <View>
 
               <View style={styles.container}>
 
                 <View style={styles.head}>
-                  <Image source={imagePath.main} style={{ height: 45, width: 45, left: 20, top: 38 }} />
-                  <Text style={{ left: 75, bottom: 45, fontWeight: 'bold', top: -10 }}> STACKGEEKS {'\n'} 1m .</Text>
+                  <Image source={imagePath.main} style={{ height: 45, width: 45, left: 20}} />
+                  <Text style={{ left: 75, bottom: 45, fontWeight: 'bold' }}> STACKGEEKS {'\n'} 1m .</Text>
 
-                  <Image source={imagePath.friends} style={{ height: 12, width: 12, left: 110, bottom: 23 }} />
-                  <TouchableOpacity onPress={this.onMode} style={styles.buttons}>
-                    <Image source={imagePath.add} style={{ height: 40, width: 40 }} />
-                  </TouchableOpacity>
+                  <Image source={imagePath.friends} style={{ height: 12, width: 12, left: 110, bottom: 58 }} />
+                 
                 </View>
 
-                <View style={{ flex: 4 }}>
-                  <Text style={{ left: 20, marginBottom: 15, color: 'black', fontWeight: 'bold' }}>{item.title}</Text>
-                  <Image source={item.images} style={{ flex: 4, width: 400, height: 500 }} />
+                <View style={{ flex: 3 }}>
+                  <Text style={{ left: 20, color: 'black', fontWeight: 'bold' }}>{item.title}</Text>
+                  <Image source={{uri : item.images}} style={{ flex: 4, width: 400, height: 500 }} />
                 </View>
-                {this.state.viewButtons ?
-                  <View style={{ flex: 0.4, backgroundColor: 'white', width: 360, flexDirection: 'row' }} >
+                  <View style={{ flex: 1, backgroundColor: 'white', width: 400, flexDirection: 'row' }} >
 
                     <View style={{ flex: 1 }}>
-                      <TouchableOpacity activeOpacity={0.4} >
-                        <Image source={imagePath.likes} style={{ height: 18, width: 18, left: 18, marginTop: 12 }} />
-                        <Text style={styles.buttonText}>Like</Text>
+                      <TouchableOpacity activeOpacity={0.4}  onPress={() => this.onLike(index)} >
+                        <Image source={imagePath.likes} style={{ height: 18, width: 18, left: 18, marginTop: 12  }} />
+                        <Text style={[styles.buttonLike , {color : this.state.bgColor}]}>Like</Text>
                       </TouchableOpacity>
                     </View>
                     <View style={{ flex: 1 }} >
-                      <TouchableOpacity activeOpacity={0.4} onPress={() => this.onEdit(index)}>
+                      <TouchableOpacity activeOpacity={0.4} onPress={() => {
+                        this.setState({val1 : item.title , val2 : item.images , index : index})
+                        this.onMode(index, item)
+                        this.state.val1 = index
+                        }}
+                        >
                         <Image source={imagePath.edit} style={{ height: 18, width: 18, left: 18, marginTop: 12 }} />
                         <Text style={styles.buttonText}>Edit</Text>
                       </TouchableOpacity>
@@ -102,34 +112,35 @@ class Feed extends Component {
                         <Image source={imagePath.delete} style={{ height: 18, width: 18, left: 12, marginTop: 12 }} />
                         <Text style={styles.buttonText}>Delete</Text></TouchableOpacity>
                     </View>
-                  </View> : null
-                }
+                  </View> 
+                
               </View>
             </View>
           }
         />
-
         <Modal visible={this.state.modale} >
-
-          <View style={{ backgroundColor: 'lightgreen', height: 600, borderRadius: 50, margin: 40, marginTop: 40 }}>
+          <View style={{ backgroundColor: 'lightgreen', height: 700, borderRadius: 50, margin: 20, marginTop: 40 }}>
             <View style={styles.modaal}>
               <View style={{ top: 60 }}>
                 <Text style={{ fontWeight: 'bold', left: 10 }} >TITLE</Text>
-                <TextInput placeholder="Enter TITLE " style={styles.placehold} onChangeText={(val1) => this.setState({ val1 })}></TextInput>
+                <TextInput placeholder="Enter TITLE " value={this.state.val1} style={styles.placehold} onChangeText={(text) => this.setState({ val1 : text })}></TextInput>
               </View>
               <View style={{ top: 100 }}>
                 <Text style={{ fontWeight: 'bold', left: 10 }}>IMAGE</Text>
-                <TextInput placeholder="Enter  URL" style={styles.placehold} onChangeText={(val2) => this.setState({ val2 })}></TextInput>
+                <TextInput placeholder="Enter  URL" value={this.state.val2} style={styles.placehold} onChangeText={(text) => this.setState({ val2 : text })}></TextInput>
               </View>
 
               <View >
-                <TouchableOpacity onPress={() => this.onSubmit()} style={{ backgroundColor: 'green', height: 40, justifyContent: 'center', borderRadius: 10, width: 230, left: 10, top: 180, borderLeftWidth: 1, borderRightWidth: 1, borderBottomWidth: 1 }}>
-                  <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'white', fontSize: 15 }}>Submit</Text>
+                <TouchableOpacity onPress={this.onSubmit}style={{ backgroundColor: 'green', height: 40, justifyContent: 'center', borderRadius: 10, width: 230, left: 6, top: 180, borderLeftWidth: 1, borderRightWidth: 1, borderBottomWidth: 1 }}>
+                  <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'white', fontSize: 15 }}>ADD NEW POST</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.onEdit}style={{ backgroundColor: 'red', height: 40, justifyContent: 'center', borderRadius: 10, width: 230, left: 6, top: 200, borderLeftWidth: 1, borderRightWidth: 1, borderBottomWidth: 1 }}>
+                  <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'white', fontSize: 15 }}>UPDATE</Text>
                 </TouchableOpacity>
               </View>
             </View>
 
-            <View style={{ left: 200, bottom: 90 }}>
+            <View style={{ left: 210, bottom: 200 }}>
               <TouchableOpacity onPress={this.offMode} style={{ bottom: 315, left: 65 }} >
                 <Image source={imagePath.cross} style={{ height: 25, width: 25 }} />
               </TouchableOpacity>
@@ -143,10 +154,7 @@ class Feed extends Component {
     )
 
   }
-
 }
-
-
 export default Feed;
 
 
@@ -157,9 +165,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  buttons: {
-    bottom: 50,
-    left: 320
+  buttons: { 
+    alignSelf : 'flex-end',
+  marginTop : 50,
+  marginRight : 20
 
   },
   placehold: {
@@ -190,12 +199,17 @@ const styles = StyleSheet.create({
 
   },
   modaal: {
-    height: 400,
+    height: 500,
     width: 280,
     padding: 20,
-    margin: 20,
+    margin: 35,
     marginTop: 80,
     backgroundColor: 'white',
     borderRadius: 50
+  },
+  buttonLike : {
+    textAlign: 'center',
+    bottom: 18,
+    fontWeight: 'bold'
   }
 });
